9f00fc288bc28b1acfa1640d7f647af9c05bc26f
hoangnh17 <hoangnh17@hdbank.com.vn>
2024-09-23 14:41:26 +0700

    build base card service for card

--- cards/src/main/java/com/microservices/cards/service/CardsService.java
+++ cards/src/main/java/com/microservices/cards/service/CardsService.java
@@ -1,8 +1,18 @@
 package com.microservices.cards.service;
 
 import com.microservices.cards.dto.CardsDto;
+import com.microservices.cards.entity.Cards;
+import com.microservices.cards.repository.CardRepository;
+
+import java.util.Optional;
 
 public class CardsService implements ICardsService {
+    private final CardRepository cardRepository;
+
+    public CardsService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
     @Override
     public void createCard(String mobileNumber) {
 
