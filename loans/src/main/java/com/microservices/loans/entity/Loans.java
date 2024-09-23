45d6abb528bc03c393e8ce161de9915f96b74729
hoangnh17 <hoangnh17@hdbank.com.vn>
2024-09-23 15:02:43 +0700

    add entity for loans

---
+++ loans/src/main/java/com/microservices/loans/entity/Loans.java
@@ -0,0 +1,34 @@
+package com.microservices.loans.entity;
+
+import com.microservices.loans.entity.base.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Loans extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String mobileNumber;
+
+    private String loanNumber;
+
+    private String loanType;
+
+    private int totalLoan;
+
+    private int amountPaid;
+
+    private int outstandingAmount;
+
+}
