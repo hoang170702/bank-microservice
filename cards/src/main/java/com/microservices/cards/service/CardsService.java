1014c59decd00296656eac67c915cd0679a9800f
hoangnh17 <hoangnh17@hdbank.com.vn>
2024-09-23 10:28:55 +0700

    add method to cards service

---
+++ cards/src/main/java/com/microservices/cards/service/CardsService.java
@@ -0,0 +1,25 @@
+package com.microservices.cards.service;
+
+import com.microservices.cards.dto.CardsDto;
+
+public class CardsService implements ICardsService {
+    @Override
+    public void createCard(String mobileNumber) {
+
+    }
+
+    @Override
+    public CardsDto fetchCard(String mobileNumber) {
+        return null;
+    }
+
+    @Override
+    public boolean updateCard(CardsDto cardsDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCard(String mobileNumber) {
+        return false;
+    }
+}
