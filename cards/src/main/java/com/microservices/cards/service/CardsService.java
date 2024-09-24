552f0326cc1463b6ff2e6e6d99e5a8f74933cad8
hoangnh17 <hoangnh17@hdbank.com.vn>
2024-09-24 11:22:17 +0700

    create card

--- cards/src/main/java/com/microservices/cards/service/CardsService.java
+++ cards/src/main/java/com/microservices/cards/service/CardsService.java
@@ -1,11 +1,16 @@
 package com.microservices.cards.service;
 
+import com.microservices.cards.constants.CardsConstants;
 import com.microservices.cards.dto.CardsDto;
 import com.microservices.cards.entity.Cards;
+import com.microservices.cards.exception.CardAlreadyExistsException;
 import com.microservices.cards.repository.CardRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Random;
 
+@Service
 public class CardsService implements ICardsService {
     private final CardRepository cardRepository;
 
@@ -15,7 +20,26 @@ public class CardsService implements ICardsService {
 
     @Override
     public void createCard(String mobileNumber) {
+        cardRepository.findByMobileNumber(mobileNumber)
+                .orElseThrow(() -> new CardAlreadyExistsException("phone number already exists" + mobileNumber));
+        cardRepository.save(createNewCard(mobileNumber));
+    }
 
+    private Cards createNewCard(String mobileNumber) {
+        Cards newCard = new Cards();
+
+        newCard.setMobileNumber(mobileNumber);
+        newCard.setCardNumber(Long.toString(createCardNumber()));
+        newCard.setCardType(CardsConstants.CREDIT_CARD);
+        newCard.setAmountUsed(0);
+        newCard.setAvailableAmount(CardsConstants.NEW_CARD_LIMIT);
+        newCard.setTotalLimit(CardsConstants.NEW_CARD_LIMIT);
+
+        return newCard;
+    }
+
+    private Long createCardNumber() {
+        return 1000000000L + new Random().nextInt(900000000);
     }
 
     @Override
