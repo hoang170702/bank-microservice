c9b2d566ce7ec59ad974c7501a885438b344ea08
HOANGNG1707 <huyhoang112022gmail.com>
2025-01-12 18:28:38 +0700

    add actuator for cards, loans, accounts

--- v2-spring-could-bank/accounts/src/main/java/com/microservices/accounts/dto/AccountsContactInfoDto.java
+++ v2-spring-could-bank/accounts/src/main/java/com/microservices/accounts/dto/AccountsContactInfoDto.java
@@ -1,13 +1,17 @@
 package com.microservices.accounts.dto;
 
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
-
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
